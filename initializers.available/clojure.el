21dcd9f351eee4b5188101044fb4f6702af1187c
Ben Mabey <ben@benmabey.com>
2010-10-18 15:59:30 -0600

    prevents annoying slime/swank version diff warning message

--- initializers.available/clojure.el
+++ initializers.available/clojure.el
@@ -12,3 +12,5 @@
 (add-hook 'emacs-lisp-mode-hook (lambda () (highlight-parentheses-mode)))
 
 (add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
+
+(eval-after-load 'slime '(setq slime-protocol-version 'ignore)) ; prevents warning
