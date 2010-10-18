8f4d647d96865f49fabd871780ba9fe1bd2a6d08
Tim Harper <timcharper@gmail.com>
2010-10-18 12:17:47 -0600

    add rails mode

---
+++ initializers.available/rails.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/rails-minor-mode"))
+(require 'rails)
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/rhtml-minor-mode/"))
+(require 'rhtml-mode)
---
+++ initializers.enabled/rails.el
@@ -0,0 +1,1 @@
+../initializers.available/rails.el
