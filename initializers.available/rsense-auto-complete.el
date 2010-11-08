cac1a6448e026743cdf03588c106608d5a917919
Ben Mabey <ben@benmabey.com>
2010-11-08 16:02:02 -0700

    adds more details on setting up rsense

--- initializers.available/rsense-auto-complete.el
+++ initializers.available/rsense-auto-complete.el
@@ -1,4 +1,7 @@
-; In order to use this you must first run "ant" in the vendor/rsense.  (This assumes you have cloned the submodules.)
+; In order to use this you must first 
+; 1. run "ant" in the vendor/rsense.  (This assumes you have cloned the submodules.)
+; 2. Set RSENSE_HOME to ~/.emacs.d/vendor/rsense in your shell file.
+; 3. After 2, run "ruby ~/.emacs.d/vendor/rsense/etc/config.rb > ~/.rsense"
 ; More info on RSense: http://cx4a.org/software/rsense/manual.html
 (setq rsense-home "~/.emacs.d/vendor/rsense")
 ;(setq rsense-home "$RSENSE_HOME")
