c6e9219b47e45caca3ab12fc2e394b12f4bea6e2
Ben Mabey <ben@benmabey.com>
2010-11-08 17:11:26 -0700

    fixes rsense initializer

--- initializers.available/rsense-auto-complete.el
+++ initializers.available/rsense-auto-complete.el
@@ -3,7 +3,7 @@
 ; 2. Set RSENSE_HOME to ~/.emacs.d/vendor/rsense in your shell file.
 ; 3. After 2, run "ruby ~/.emacs.d/vendor/rsense/etc/config.rb > ~/.rsense"
 ; More info on RSense: http://cx4a.org/software/rsense/manual.html
-(setq rsense-home "~/.emacs.d/vendor/rsense")
+(setq rsense-home (expand-file-name "~/.emacs.d/vendor/rsense"))
 ;(setq rsense-home "$RSENSE_HOME")
 (add-to-list 'load-path (concat rsense-home "/etc"))
 (require 'rsense)
