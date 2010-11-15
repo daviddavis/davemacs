67e9a15ef6d8ac11b73acfb5711df56bd53f2237
Joe Tanner <joe.tanner@leadmediapartners.com>
2010-11-15 12:06:18 -0700

    adds window-rotate command

---
+++ initializers.available/window-rotate.el
@@ -0,0 +1,11 @@
+(defun rotate-windows-helper(x d)
+  (if (equal (cdr x) nil) (set-window-buffer (car x) d)
+    (set-window-buffer (car x) (window-buffer (cadr x))) (rotate-windows-helper (cdr x) d)))
+ 
+(defun rotate-windows ()
+  (interactive)
+  (rotate-windows-helper (window-list) (window-buffer (car (window-list))))
+  (select-window (car (last (window-list)))))
+
+; bind to the key ctrl-R 
+(global-set-key (kbd "C-S-r") 'rotate-windows)
