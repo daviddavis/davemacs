ed23bc8061f0eb8d2d56475c0ac5464164663c8e
Tim Harper <timcharper@gmail.com>
2011-02-21 17:24:39 -0700

    ess - don't load by default

--- initializers.available/ess.el
+++ initializers.available/ess.el
@@ -3,8 +3,7 @@
 
 (autoload 'R-mode "ess-site" "" t nil)
 (autoload 'R "ess-site" "" t nil)
-(add-to-list 'load-path "~/.emacs.d/vendor/vr-mode")
-;; (load-file "~/.emacs.d/vendor/ess-5.5/lisp/ess-site.el")
+(load-file "~/.emacs.d/vendor/ess-5.5/lisp/ess-site.el")
 
 ;; https://stat.ethz.ch/pipermail/ess-help/2009-February/005158.html
 (setq ess-eval-visibly-p nil)
--- initializers.enabled/ess.el
+++
@@ -1,1 +0,0 @@
-../initializers.available/ess.el
