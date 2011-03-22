70eb4e0e24aa5fca54cd8e766ff9d1802d0c7e4e
Tim Harper <timcharper@gmail.com>
2011-03-22 10:41:13 -0600

    upgrade auto-complete

--- initializers.available/auto-complete.el
+++ initializers.available/auto-complete.el
@@ -1,4 +1,4 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/auto-complete")
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/ac-dict")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/dict")
 (ac-config-default)
